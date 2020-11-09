7bd8ba40d83cf0ab6e59f98264a72e95ae63be70
woodspring <woodspring.toronto@gmail.com>
2020-11-08 20:54:35 -0500

    first added

---
+++ src/test/java/woodspring/springkafkaprovider/SpringKafkaProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package woodspring.springkafkaprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
